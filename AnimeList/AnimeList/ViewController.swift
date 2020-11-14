9e9a77c4f46bc2007cbc91e710fdb751c06162b4
Ann <ann330001@gmail.com>
2020-11-14 10:37:58 +0800

    ver 1.0.0
    add:
    - start project

---
+++ AnimeList/AnimeList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimeList
+//
+//  Created by Ann on 2020/11/14.
+//  Copyright © 2020 AnnChen.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
